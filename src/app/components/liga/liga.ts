a640185e8386b630394f02d4d1e12e335d34bd42
Borquez15 <aborquez1551@gmail.com>
2025-10-01 21:29:47 -0700

    Initial commit

---
+++ src/app/components/liga/liga.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liga',
+  imports: [],
+  templateUrl: './liga.html',
+  styleUrl: './liga.css'
+})
+export class Liga {
+
+}
