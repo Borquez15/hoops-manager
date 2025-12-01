15995934a396b53a0e3a1b0a1ad7c679e585aa84
Borquez15 <aborquez1551@gmail.com>
2025-12-01 14:09:52 -0700

    hosteando

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,  // Para producción
+  firebase: {
+    apiKey: 'AIzaSyCFBZMRfbzsTrUmyRX8XNOltqzSLK0-p3A',
+    authDomain: 'hoopsmanager-7fd19.firebaseapp.com',
+    projectId: 'hoopsmanager-7fd19',
+    storageBucket: 'hoopsmanager-7fd19.appspot.com',
+    messagingSenderId: '91417343471',
+    appId: '1:91417343471:web:9abef41b66cf9e17b7321a',
+  },
+  apiBase: 'https://hoopsbackend-production.up.railway.app/'  // ← URL de Railway
+};
